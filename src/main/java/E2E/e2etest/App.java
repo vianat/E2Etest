dd128cfe75da78740bef249317dd3d0cf1e5c184
Sergei Nikiforov <vianatdev@gmail.com>
2022-11-01 13:12:57 -0700

    git inittiated

---
+++ src/main/java/E2E/e2etest/App.java
@@ -0,0 +1,13 @@
+package E2E.e2etest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
