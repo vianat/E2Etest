db7028263b58e2927bba12be5194dfcc25840e81
vianat <vianatdev@gmail.com>
2022-11-06 16:53:15 -0800

    re move

--- src/test/java/StepDefinitions/TestRunner.java
+++ src/test/java/StepDefinitions/TestRunner.java
@@ -5,7 +5,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/java/resources/Features", glue = { "StepDefinitions" },
+@CucumberOptions(features = "src/test/java//FeaturesFiles", glue = { "StepDefinitions" },
 monochrome=true,
 plugin= {"pretty", "html:target/htmlReports/report.html",
 		"json:target/JsonReports/report.json",
