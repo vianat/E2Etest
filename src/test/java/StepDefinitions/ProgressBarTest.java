f97a1763e5d34c5cbff4c83a170628843c7bb8fe
Azik1994 <utr1994@mail.ru>
2022-11-19 12:21:34 -0800

    progressbar

---
+++ src/test/java/StepDefinitions/ProgressBarTest.java
@@ -0,0 +1,32 @@
+package StepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import org.testng.annotations.Test;
+import pageObjects.ProgressBarPage;
+import resources.base;
+
+import java.time.Duration;
+
+@Test
+public class ProgressBarTest extends base {
+
+    ProgressBarPage pbp = new ProgressBarPage(driver);
+
+    @And("Click start button and wait {int} mseconds")
+    public void clickStartButtonAndWaitSeconds(int time) throws InterruptedException {
+        pbp.startandstopBtn.click();
+       Thread.sleep(time);
+    }
+
+    @And("Click stop button")
+    public void clickStopButton() {
+        pbp.startandstopBtn.click();
+    }
+
+    @Then("Get progress percentage")
+    public void getProgressPercentage() {
+        System.out.println("****"+ pbp.progressmessage.getText());
+    }
+}
