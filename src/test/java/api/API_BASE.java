3372f6bc8f00a08438a9080e90d486efdeaa8293
vianat <vianatdev@gmail.com>
2022-11-25 20:45:39 -0800

    add login request

--- src/test/java/api/API_BASE.java
+++ src/test/java/api/API_BASE.java
@@ -1,11 +1,33 @@
 package api;
 
+import api.pojo.LoginRequest;
+import api.pojo.LoginResponse;
 import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import static io.restassured.RestAssured.given;
 
 public class API_BASE {
 
     public static void main(String[] args) {
 
-        new RequestSpecBuilder().setBaseUri("https://rahulshettyacademy.com/api/");
+        RequestSpecification REQ = new RequestSpecBuilder()
+                                        .setBaseUri("https://rahulshettyacademy.com/api/ecom/")
+                                        .setContentType(ContentType.JSON)
+                                        .build();
+
+        LoginRequest payLoad = new LoginRequest();
+        payLoad.setUserEmail("nik.seey87@mail.ru");
+        payLoad.setUserPassword("3LuvREk3M7GKe@n");
+
+        RequestSpecification reqLogin = given().log().all().spec(REQ).body(payLoad);
+
+        LoginResponse loginResponse = reqLogin
+                .when().post("/auth/login")
+                .then().log().all().extract().response().as(LoginResponse.class);
+
+        System.out.println(loginResponse.getToken());
+        System.out.println(loginResponse.getUserId());
+        System.out.println(loginResponse.getMessage());
     }
-}
+}   
