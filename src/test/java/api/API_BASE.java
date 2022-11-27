13070892e709667f2d1bedeb5cb95554efb9fdd2
vianat <vianatdev@gmail.com>
2022-11-26 17:05:07 -0800

    delete request

--- src/test/java/api/API_BASE.java
+++ src/test/java/api/API_BASE.java
@@ -24,19 +24,19 @@ public class API_BASE {
         // Authorization
 
         RequestSpecification loginSPEC = new RequestSpecBuilder()
-                                                .setBaseUri("https://rahulshettyacademy.com/api/ecom/")
-                                                .setContentType(ContentType.JSON)
-                                                .build();
+                .setBaseUri("https://rahulshettyacademy.com/api/ecom/")
+                .setContentType(ContentType.JSON)
+                .build();
 
         LoginREQ loginPayload = new LoginREQ();
         loginPayload.setUserEmail("nik.seey87@mail.ru");
         loginPayload.setUserPassword("3LuvREk3M7GKe@n");
 
-        RequestSpecification loginREQ = given().log().all().spec(loginSPEC).body(loginPayload);
+        RequestSpecification loginREQ = given().relaxedHTTPSValidation().log().all().spec(loginSPEC).body(loginPayload);
 
         LoginRESP loginRESP = loginREQ
-                                .when().post("/auth/login")
-                                .then().log().all().extract().response().as(LoginRESP.class);
+                .when().post("/auth/login")
+                .then().log().all().extract().response().as(LoginRESP.class);
 
         userId = loginRESP.getUserId();
         token = loginRESP.getToken();
@@ -45,9 +45,9 @@ public class API_BASE {
         // Add product
 
         RequestSpecification addProductSPEC = new RequestSpecBuilder()
-                                                    .setBaseUri("https://rahulshettyacademy.com/api/ecom/")
-                                                    .addHeader("authorization", token)
-                                                    .build();
+                .setBaseUri("https://rahulshettyacademy.com/api/ecom/")
+                .addHeader("authorization", token)
+                .build();
 
         RequestSpecification addProductREQ = given().log().all().spec(addProductSPEC)
                 .params("productCategory", "fashion")
@@ -66,8 +66,8 @@ public class API_BASE {
 
 // as string
         String addProductRESP = addProductREQ
-                                    .when().post("/product/add-product")
-                                    .then().log().all().extract().response().asString();
+                .when().post("/product/add-product")
+                .then().log().all().extract().response().asString();
 
         JsonPath js = new JsonPath(addProductRESP);
         productId = js.getString("productId");
@@ -75,11 +75,11 @@ public class API_BASE {
 
     // Create order
 
-        RequestSpecification createOrderSPEC = new RequestSpecBuilder()
-                                                        .setBaseUri("https://rahulshettyacademy.com/api/ecom/")
-                                                        .addHeader("authorization", token)
-                                                        .setContentType(ContentType.JSON)
-                                                        .build();
+        RequestSpecification SPEC = new RequestSpecBuilder()
+                .setBaseUri("https://rahulshettyacademy.com/api/ecom/")
+                .addHeader("authorization", token)
+                .setContentType(ContentType.JSON)
+                .build();
 
         Orders createOrderPayload = new Orders();
         Order order = new Order();
@@ -91,14 +91,32 @@ public class API_BASE {
         orderList.add(order);
         createOrderPayload.setOrders(orderList);
 
-        RequestSpecification createOrderREQ = given().log().all().spec(createOrderSPEC).body(createOrderPayload);
+        RequestSpecification createOrderREQ = given().log().all().spec(SPEC).body(createOrderPayload);
 
         String createOrderRESP = createOrderREQ
-                                    .when().post("/order/create-order")
-                                    .then().log().all().extract().response().asString();
+                .when().post("/order/create-order")
+                .then().log().all().extract().response().asString();
 
         js = new JsonPath(createOrderRESP);
-        ordersId = js.getString("ordersId");
+        ordersId = js.getString("orders");
+        ordersId = ordersId.substring( 1, ordersId.length() - 1 );
 
+
+        // Get order
+
+        RequestSpecification getOrderREQ = given().log().all().spec(SPEC);
+
+        String getOrderRESP = getOrderREQ
+                .when().get("/order/get-orders-details?id=" + ordersId)
+                .then().log().all().extract().response().asString();
+
+
+        // Delete product
+
+        RequestSpecification deleteProductREQ = given().log().all().spec(SPEC).pathParams("productId", productId);
+
+        deleteProductREQ
+                .when().delete("product/delete-product/{productId}")
+                .then().log().all();
     }
 }   
